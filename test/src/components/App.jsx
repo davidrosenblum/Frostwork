d086aa55b9f149c12f0b3e9e607237149b1de074
David Rosenblum <david@rosenblum.net>
2018-12-13 16:17:06 -0500

    Remove tests

--- test/src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export class App extends React.Component{
-    render(){
-        <div>
-            <div id="canvas-container"></div>
-        </div>
-    }
-}
--- test/src/game/Game.js
+++
@@ -1,7 +0,0 @@
-import * as fw from "@davidrosenblum/frostwork";
-
-export class Game extends fw.Game{
-    constructor(){
-
-    }
-}
